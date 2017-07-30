6c02ab6b2029e5b4aeb7a10edf95c864046e6c16
Paul Wagner <paul.wagner@iki.fi>
2017-07-30 17:20:45 +0300

    Copy DLL loader code from branch z/exploration/import-old-plugin
    Copy from commit a551c4399f8b635aae1ad8e7ed63830bde5bdfde

--- Source/UnrealTorch/Public/UnrealTorch.h
+++ Source/UnrealTorch/Public/UnrealTorch.h
@@ -1,9 +1,16 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "ModuleManager.h"
+
+#include <string>
+#include <vector>
+
+
+DECLARE_LOG_CATEGORY_EXTERN( LogUnrealTorch, Log, All );
+
 
 class FUnrealTorchModule : public IModuleInterface
 {
@@ -12,4 +19,20 @@ public:
 	/** IModuleInterface implementation */
 	virtual void StartupModule() override;
 	virtual void ShutdownModule() override;
+
+
+private:
+
+	/** A single dll that we will load */
+	struct ThirdPartyDll
+	{
+		std::string name;
+		void * handle;
+	};
+
+	/** Torch dlls
+	 * 
+	 * We cannot move this into the Torch module, because including it into the .uplugin file would cause UBT to try to
+	 * (re)compile also all C files from Torch itself. */
+	static std::vector<ThirdPartyDll> thirdPartyDlls;
 };
--- Source/UnrealTorch/UnrealTorch.Build.cs
+++ Source/UnrealTorch/UnrealTorch.Build.cs
@@ -1,13 +1,20 @@
 // Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
 
 using UnrealBuildTool;
+using System.IO;
 
 public class UnrealTorch : ModuleRules
 {
 	public UnrealTorch(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
+
+		// Including Sol2 without this gives the error C4577 from <utility>
+		// Alternative solution: disable 4577 in WindowsPlatformCompilerSetup.h
+		//   (disabling 4577 in your own code is not enough, for some reason)
+		// Also, we now actually use exceptions as they now work with the latest versions of everything.
+		bEnableExceptions = true;
+
 		PublicIncludePaths.AddRange(
 			new string[] {
 				"UnrealTorch/Public"
@@ -28,6 +35,10 @@ public class UnrealTorch : ModuleRules
 			new string[]
 			{
 				"Core",
+				"Projects",
+				"RangeV3",
+				"Sol2",
+				"Torch",
 				// ... add other public dependencies that you statically link with here ...
 			}
 			);
@@ -51,5 +62,14 @@ public class UnrealTorch : ModuleRules
 				// ... add any modules that your module loads dynamically here ...
 			}
 			);
+
+
+		// Have our Lua files included during packaging
+		//
+		// cd Source/UnrealTorch/
+		// find Private/lua/ -type f | sed 's_\(.*\)_RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(ModuleDirectory, "\1")));_'
+		//RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(ModuleDirectory, "Private/lua/startup_module.lua")));
+		//RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(ModuleDirectory, "Private/lua/uth/tests.lua")));
+		//RuntimeDependencies.Add(new RuntimeDependency(Path.Combine(ModuleDirectory, "Private/lua/uth/utility.lua")));
 	}
 }
