955877aeb12bbb0a66effb0868ef2db64b486073
Paul Wagner <paul.wagner@iki.fi>
2017-08-01 13:32:00 +0300

    Create UthLuaState class from Object

---
+++ Source/UnrealTorch/Public/UthLuaState.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "UthLuaState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTORCH_API UUthLuaState : public UObject
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
