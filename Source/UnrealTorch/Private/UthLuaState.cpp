b996a82aab8a54d05fbe42df94d13d6e16ec1a21
Paul Wagner <paul.wagner@iki.fi>
2017-08-02 10:48:46 +0300

    UthLuaState: Implement class construction
    Create sol::state.
    Open all Lua standard libraries.
    Set Lua paths.

--- Source/UnrealTorch/Private/UthLuaState.cpp
+++ Source/UnrealTorch/Private/UthLuaState.cpp
@@ -2,6 +2,50 @@
 
 #include "UthLuaState.h"
 
+#include "IPluginManager.h"
+#include "Paths.h"
+
+#include "UEWrappedSol2.h"
+
+#include <string>
+#include <memory>
 
 
 
+
+UUthLuaState::UUthLuaState()
+{}
+
+
+void UUthLuaState::construct()
+{
+	// make sure that we don't yet have a Lua state
+	checkf( !lua, TEXT("Lua state already exists! Maybe the construct() method has been already called?") );
+
+
+	// Get base directories
+	std::string BaseDirPlugin = TCHAR_TO_UTF8( *IPluginManager::Get().FindPlugin( "UnrealTorch" )->GetBaseDir() );
+	std::string BaseDirGameContent = TCHAR_TO_UTF8( *FPaths::GameContentDir() );
+	std::string BaseDirGameLogs = TCHAR_TO_UTF8( *FPaths::GameLogDir() );
+
+	// create a sol-wrapped Lua state
+	lua = std::make_unique<sol::state>();
+
+	// Open all standard libraries
+	lua->open_libraries();
+
+	// Set paths
+	(*lua)["package"]["path"] =
+		BaseDirPlugin + "/Source/UnrealTorch/Private/lua/?.lua;" +
+		BaseDirPlugin + "/Source/ThirdParty/Torch/WindowsTorch/lua/?.lua;" +
+		BaseDirPlugin + "/Source/ThirdParty/Torch/WindowsTorch/lua/?/init.lua;" +
+		BaseDirGameContent + "/Lua/?.lua;" +
+		BaseDirGameContent + "/Lua/?/init.lua";   // no semicolon at end to avoid getting ;; by accident later
+	(*lua)["package"]["cpath"] =
+		BaseDirPlugin + "/Source/ThirdParty/Torch/WindowsTorch/bin/?.dll;" +
+		BaseDirGameContent + "/Lua/bin/?.dll";   // no semicolon at end to avoid getting ;; by accident later
+}
+
+
+UUthLuaState::~UUthLuaState()
+{}
--- Source/UnrealTorch/Public/UthLuaState.h
+++ Source/UnrealTorch/Public/UthLuaState.h
@@ -4,17 +4,65 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+
+#include "UEWrappedSol2.h"    // unique_ptr and forward declarations do not seem to play with UBT..
+
+#include <memory>
+
 #include "UthLuaState.generated.h"
+
+
+
 
 /**
  * 
  */
-UCLASS()
+UCLASS( Category = "UnrealTorch|Lua", meta = (DisplayName = "Lua State") )
 class UNREALTORCH_API UUthLuaState : public UObject
 {
 	GENERATED_BODY()
-	
-	
-	
-	
+
+public:
+
+	/** Construct a new UthLuaState object but do not initialize it in any way. Call construct() before using.
+	 *
+	 * We cannot create a Lua state yet in the constructor, because UE will construct instances of the class before the
+	 * Lua dlls have been loaded.
+	 **/
+	UUthLuaState();
+
+	/** Late-construct the object, creating a new Lua state and performing some initialization on it.
+	*
+	* A new sol::state is created.
+	*
+	* All Lua standard libraries are opened.
+	*
+	* A global table 'uth' is created with the following structure:
+	*   uth                                  The main table for all UnrealTorch data
+	*     .ue                                Data from UE
+	*       .BuildShippingOrTest             True if UE is running in Shipping or Test configuration
+	*       .FPaths                          UE path information
+	*         .GameLogDir                    Log directory path from FPaths::GameLogDir()
+	*       .UE_LOG( verbosity, message )    Write log entries to UE log
+	*       .ELogVerbosity                   Verbosity level enumeration for UE_LOG()
+	*
+	* The package.path variable is set to include the following locations, in this order:
+	*   1. UnrealTorch's own Lua files: <pluginroot>/Source/UnrealTorch/Private/lua/?.lua
+	*   2. Torch's Lua files: <pluginroot>/Source/ThirdParty/Torch/WindowsTorch/lua/?.lua and
+	*      <pluginroot>/Source/ThirdParty/Torch/WindowsTorch/lua/?/init.lua
+	*   3. Project level Lua files: <root>/Content/Lua/?.lua and <root>/Content/Lua/?/init.lua
+	* The package.cpath variable is set to include the following locations, in this order:
+	*   1. Torch DLLs: <pluginroot>/Source/ThirdParty/Torch/WindowsTorch/bin/?.dll
+	*   2. Project level DLLs: <root>/Content/Lua/bin/?.dll
+	*/
+	void construct();
+
+	/** No-op. */
+	virtual ~UUthLuaState();
+
+
+
+	/** Sol-wrapped Lua state instance */
+	std::unique_ptr<sol::state> lua;
+
 };
