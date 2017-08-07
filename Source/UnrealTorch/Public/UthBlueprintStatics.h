113e20293059476ad42fda985f2eaea191def7df
Paul Wagner <paul.wagner@iki.fi>
2017-08-07 12:31:02 +0300

    UthBlueprintStatics: Create a new Blueprint function library

---
+++ Source/UnrealTorch/Public/UthBlueprintStatics.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UthBlueprintStatics.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALTORCH_API UUthBlueprintStatics : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
