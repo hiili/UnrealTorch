c5b3db4ad0c3d08a76f364c31c79cc5d43abcd98
Paul Wagner <paul.wagner@iki.fi>
2017-08-08 15:08:22 +0300

    UthBlueprintStatics (CreateLuaState): Add static factory function for Blueprint use

--- Source/UnrealTorch/Private/UthBlueprintStatics.cpp
+++ Source/UnrealTorch/Private/UthBlueprintStatics.cpp
@@ -2,6 +2,21 @@
 
 #include "UthBlueprintStatics.h"
 
+#include "UthLuaState.h"
 
 
 
+
+UUthLuaState * UUthBlueprintStatics::CreateLuaState( FName name /*= FName( "default" ) */ )
+{
+	UUthLuaState * lua = NewObject<UUthLuaState>( GetTransientPackage(), FName(), RF_MarkAsRootSet );    // don't interfere with the UObject name system
+	if( !lua ) return nullptr;
+	if( !lua->isValid() )
+	{
+		lua->destroy();
+		return nullptr;
+	}
+
+	lua->setName( name );
+	return lua;
+}
--- Source/UnrealTorch/Public/UthBlueprintStatics.h
+++ Source/UnrealTorch/Public/UthBlueprintStatics.h
@@ -6,6 +6,12 @@
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "UthBlueprintStatics.generated.h"
 
+
+class UUthLuaState;
+
+
+
+
 /**
  * 
  */
@@ -13,9 +19,43 @@ UCLASS()
 class UNREALTORCH_API UUthBlueprintStatics : public UBlueprintFunctionLibrary
 {
 	GENERATED_BODY()
-	
+
 public:
-	
-	
+
+	// comment block copy at UUthLuaState::UUthLuaState()
+	/** Constructs a new UthLuaState object, creating a new Lua state and initializing it.
+	 *
+	 * A new Lua state is created and initialized with all Torch-related paths set, necessary packages and dlls loaded,
+	 * and a set of utility functions defined.
+	 *
+	 * All Lua standard libraries are opened.
+	 *
+	 * A global variable 'uth' is created with the following structure:
+	 *   uth								The main table for all UnrealTorch data
+	 *     .statename						Name of this state; see UUthLuaState.setName()
+	 *     .ue								Data from UE
+	 *       .UE_LOG( verbosity, message )	Write log entries to UE log
+	 *       .ELogVerbosity					Verbosity level enumeration for UE_LOG()
+	 *       .BuildShippingOrTest			True if UE is running in Shipping or Test configuration
+	 *       .FPaths						UE path information
+	 *         .GameLogDir					Log directory path from FPaths::GameLogDir()
+	 *
+	 * The package.path variable is set to include the following locations, in this order:
+	 *   1. UnrealTorch Lua files
+	 *   2. Torch Lua files
+	 *   3. Project level Lua files: Content/Lua/?.lua and Content/Lua/?/init.lua
+	 *
+	 * The package.cpath variable is set to include the following locations, in this order:
+	 *   1. Torch DLLs
+	 *   2. Project level DLLs: Content/Lua/bin/?.dll
+	 *
+	 * Object lifecycle and garbage collection:
+	 *
+	 * When a new Lua state is created using the Blueprint helper function CreateLuaState(), it will be added to the
+	 * root set of UObjects and thus excluded from garbage collection. Use UUthLuaState::destroy() to send it toward
+	 * destruction. It is safe to call RemoveFromRoot() on the created object if you wish to put it under GC.
+	 */
+	UFUNCTION( BlueprintCallable, Category = "Unreal Torch|Lua" )
+	static UUthLuaState * CreateLuaState( FName name = FName( "default" ) );
 	
 };
