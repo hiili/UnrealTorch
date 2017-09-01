5daa3aadd374d40ac369ed0de0b76723ac747629
Paul Wagner <paul.wagner@iki.fi>
2017-09-01 18:56:05 +0300

    [test][cleanup] Description strings
    Modify to make sense in the automation test output context.

--- Source/UnrealTorch/Private/Tests/UthLuaStateTest.cpp
+++ Source/UnrealTorch/Private/Tests/UthLuaStateTest.cpp
@@ -28,11 +28,15 @@ bool FUthLuaStateTest::RunTest( const FString & Parameters )
 		UUthLuaState * luaC1{ NewObject<UUthLuaState>( GetTransientPackage(), FName() ) };    // becomes a dangling pointer after next GC round
 		UUthLuaState * luaC2{ NewObject<UUthLuaState>( GetTransientPackage(), FName(), RF_MarkAsRootSet ) };
 
-		TestNotNull( TEXT( "UthLuaState created in C++ via NewObject() is not null" ), luaC1 );
-		TestNotNull( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSet flag is not null " ), luaC2 );
+		TestNotNull( TEXT( "UthLuaState created in C++ via NewObject()" ),
+					 luaC1 );
+		TestNotNull( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSet flag" ),
+					 luaC2 );
 
-		TestTrue( TEXT( "UthLuaState created in C++ via NewObject(): isValid() == true" ), luaC1->isValid() );
-		TestTrue( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSet flag: isValid() == true" ), luaC2->isValid() );
+		TestTrue( TEXT( "UthLuaState created in C++ via NewObject(): isValid()" ),
+				  luaC1->isValid() );
+		TestTrue( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSet flag: isValid()" ),
+				  luaC2->isValid() );
 
 		TestEqual( TEXT( "UthLuaState created in C++ via NewObject(): getName() == 'default'" ),
 				   luaC1->getName(),
@@ -52,8 +56,10 @@ bool FUthLuaStateTest::RunTest( const FString & Parameters )
 		luaC1->destroy(); luaC1 = nullptr;
 		luaC2->destroy(); luaC2 = nullptr;
 
-		TestFalse( TEXT( "UthLuaState created in C++ via NewObject(): destroy() => isValid() == false" ), luaC1_PendingKill->isValid() );
-		TestFalse( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSet flag: destroy() => isValid() == false" ), luaC2_PendingKill->isValid() );
+		TestFalse( TEXT( "UthLuaState created in C++ via NewObject(): destroy() => isValid()" ),
+				   luaC1_PendingKill->isValid() );
+		TestFalse( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSet flag: destroy() => isValid()" ),
+				   luaC2_PendingKill->isValid() );
 	}
 
 	// Object lifetime and garbage collection
@@ -66,24 +72,28 @@ bool FUthLuaStateTest::RunTest( const FString & Parameters )
 		luaC1 = nullptr;
 
 		// (*luaC1) should be dead now, but we cannot test it without risking a crash
-		TestTrue( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSet flag: CollectGarbage() => isValid() == true" ), luaC2->isValid() );    // Might crash if luaC2 was nevertheless collected. Setting full purge to false does not help.
+		TestTrue( TEXT( "UthLuaState created in C++ via NewObject() with MarkAsRootSet flag: CollectGarbage() => isValid()" ),
+				  luaC2->isValid() );    // Might crash if luaC2 was nevertheless collected. Setting full purge to false does not help.
 
 		luaC2->destroy(); luaC2 = nullptr;
 	}
 
-	// UUthLuaState::script()
+	// script()
 	{
 		UUthLuaState * lua{ NewObject<UUthLuaState>( GetTransientPackage(), FName(), RF_MarkAsRootSet ) };
 		check( lua && lua->isValid() );
 
-		TestTrue( TEXT( "UthLuaState::script(<valid expression>) -> true" ), lua->script("x1 = 1 + 1") );
+		TestTrue( TEXT( "UthLuaState::script(<valid expression>) return status" ),
+				  lua->script("x1 = 1 + 1") );
 
 		AddExpectedError( TEXT( ".UUthLuaState::script. Failed to run script: runtime error .string .x2 = 1 . 1 . initially_undefined_variable..:1: attempt to perform arithmetic on global 'initially_undefined_variable' .a nil value." ),
 						  EAutomationExpectedErrorFlags::Exact, 1 );
-		TestFalse( TEXT( "UthLuaState::script(<invalid expression>) -> false" ), lua->script( "x2 = 1 + 1 + initially_undefined_variable" ) );
+		TestFalse( TEXT( "UthLuaState::script(<invalid expression>) return status && expected error detected" ),
+				   lua->script( "x2 = 1 + 1 + initially_undefined_variable" ) );
 
 		lua->script( "initially_undefined_variable = 10" );
-		TestTrue( TEXT( "UthLuaState::script(<set x>); UthLuaState::script(<use x>) -> true" ), lua->script( "x3 = 1 + 1 + initially_undefined_variable" ) );
+		TestTrue( TEXT( "UthLuaState::script(<set x>); UthLuaState::script(<use x>) return status" ),
+				  lua->script( "x3 = 1 + 1 + initially_undefined_variable" ) );
 
 		lua->destroy(); lua = nullptr;
 	}
@@ -94,7 +104,7 @@ bool FUthLuaStateTest::RunTest( const FString & Parameters )
 		check( lua && lua->isValid() );
 
 		AddExpectedError( TEXT( ".. Test error message from Lua via UE_LOG" ), EAutomationExpectedErrorFlags::Exact, 1 );
-		TestTrue( TEXT( "UthLuaState: Lua call to uth.ue.UE_LOG() works." ),
+		TestTrue( TEXT( "UthLuaState: Lua call to uth.ue.UE_LOG( <error>, <error message> ) return status && expected error detected" ),
 				  lua->script( "uth.ue.UE_LOG( uth.ue.ELogVerbosity.Error, 'Test error message from Lua via UE_LOG' )" ) );
 
 		lua->destroy(); lua = nullptr;
@@ -129,7 +139,7 @@ bool FUthLuaStateTest::RunTest( const FString & Parameters )
 		std::string logLine;
 		for( auto & expectedLogLine : expectedLogLines )
 		{
-			TestTrue( TEXT( "UthLuaState: Lua calls to print(), io.write() and LOG() cause expected output in the log file." ),
+			TestTrue( TEXT( "UthLuaState: Lua calls to print(), io.write() and LOG() cause expected output in the log file" ),
 					  getline( logFile, logLine ) && logLine == expectedLogLine );
 		}
 
